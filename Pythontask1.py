3a7c295effe0a9dcd0a5adcaae833dcf465cccab
pandigeethak <72066204+pandigeethak@users.noreply.github.com>
2021-09-28 16:02:44 +0530

    Add files via upload
    I have completed Task 1 ( Display Student Mark with name )

---
+++ Pythontask1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+username = input("Enter Studentname:")
+print("Username is  " +  username)
+Subject1 = float(input(" Enter mark "))
+Subject2 = float(input(" Enter mark "))
+Subject3 = float(input(" Enter mark "))
+Subject4 = float(input(" Enter mark "))
+Subject5 = float(input(" Enter mark "))
+total=Subject1+ Subject2+ Subject3+ Subject4+Subject5
+Average = total/5
+print("Total Marks = %.2f"  %total)
+print("Average Marks = %.2f"  %Average)
+
+
+
+            
+
